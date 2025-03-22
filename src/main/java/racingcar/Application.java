9f9072c5af9baabd635c872396cb1b7659494e4f
NIF404 <dlgkdnjs2418@gmail.com>
2025-03-22 09:58:26 +0900

    feat:입력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차를 입력 하세요 : ");
+        String member_input = Console.readLine();
+
+        System.out.println("시도 횟수를 입력하세요 : ");
+        int try_count = Integer.parseInt(Console.readLine());
+
+        member_input = member_input.replace(" ", "");
+        String[] player = member_input.split(",");
+
+
     }
 }
