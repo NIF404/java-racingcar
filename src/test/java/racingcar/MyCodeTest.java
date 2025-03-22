80a6b6ae4ca4566bcdce12755d9994f559b7f815
NIF404 <dlgkdnjs2418@gmail.com>
2025-03-22 19:38:34 +0900

    feat : 최종

---
+++ src/test/java/racingcar/MyCodeTest.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class MyCodeTest {
+}
