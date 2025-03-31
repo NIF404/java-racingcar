60211fb675dc817129152a7a843007082a4a814b
NIF404 <dlgkdnjs2418@gmail.com>
2025-03-31 18:57:01 +0900

    refactor : 'RacingEvent'클래스 생성 및 자동차 추가 메소드 작성

--- src/main/java/racingcar/RacingEvent.java
+++ src/main/java/racingcar/RacingEvent.java
@@ -1,11 +1,43 @@
 package racingcar;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RacingEvent {
 
+    private final List<Car> players = new ArrayList<>();
 
 
     public RacingEvent(){
+    }
 
+    public RacingEvent(Car[] cars){
+        addAllCar(cars);
+    }
+
+
+
+    public void addCar(Car car){
+        if(players.contains(car)){
+            System.out.println("이미 참여한 자동차 입니다.");
+        }
+        else players.add(car);
+    }
+
+    public void addAllCar(Car[] cars){
+        for(Car car : cars){
+            if(!players.contains(car)){
+                players.add(car);
+            }
+        }
+
+        System.out.println("이미 참여한 자동차를 제외한 나머지를 추가했습니다");
+    }
+
+    public void removeCar(Car car){
+        if(!players.contains(car)){
+            System.out.println("참여하지 않은 자동차 입니다.");
+        }
+        players.remove(car);
     }
 
 }
